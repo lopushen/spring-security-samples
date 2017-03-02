5ef77cb14961372d8b1e44dcf8252751db44e061
Ievgenii Lopushen <lopushen@gmail.com>
2017-03-03 01:00:23 +0200

    Base classes for tutorial

--- src/main/java/com/lopushen/config/WebSecurityConfig.java
+++ src/main/java/com/lopushen/config/WebSecurityConfig.java
@@ -1,18 +1,36 @@
 package com.lopushen.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@EnableWebSecurity
-public class WebSecurityConfig {
+//@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
         userDetailsManager.createUser(User.withUsername("Anna").password("11111").roles("USER").build());
         userDetailsManager.createUser(User.withUsername("Ievgenii").password("55555").roles("USER,ADMIN").build());
         return userDetailsManager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
     }
 }
