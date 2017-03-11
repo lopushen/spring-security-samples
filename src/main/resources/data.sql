5120946dfd491521a8672972e99c7fcd4728d5c6
Ievgenii Lopushen <lopushen@gmail.com>
2017-03-11 21:22:02 +0200

    added initial data for security

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `users` (
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(20) NOT NULL DEFAULT '',
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `roles` (
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `role` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`username`,`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users` (`username`, `password`, `enabled`)
+VALUES
+	('ievgeniii', '21111991', 1);
+
+INSERT INTO `roles` (`username`, `role`)
+VALUES
+	('ievgeniii', 'User'),
+	('ievgeniii', 'Admin');
+
+commit;
